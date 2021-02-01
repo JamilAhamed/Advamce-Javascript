fb8e22c6316ac380cca2fb992bf2a281363200c5
Your Name <you@example.com>
2021-02-01 23:32:22 +0600

    adding true / false

---
+++ true-false.js
@@ -0,0 +1,22 @@
+// const name = 0;
+// falsy;
+// 0
+// ""
+// undefind
+// null
+// NaN
+
+// truthy
+// "0"
+// ' '
+// []
+
+
+let name = 0;
+
+if(name || name == 0){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
