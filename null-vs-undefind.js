4df712905cb960a1fbf32256f0c1c9514e485521
Your Name <you@example.com>
2021-02-02 00:05:47 +0600

    null vs undefind

---
+++ null-vs-undefind.js
@@ -0,0 +1,13 @@
+let pakhi;
+function add(num1, num2){
+    console.log(num1 + num2);
+    return aikhane kisu return na korle undefind asbe
+}
+function add( num1, num2){
+    console.log(num1, num2);
+}
+ const boltu ={name: "Smart Dude",Address: kochukhet};
+ console.log(boltu.gf); aikhane undefind dekhabe
+  
+let ages =[2, 5, 9]
+console.log(ages[121]);
